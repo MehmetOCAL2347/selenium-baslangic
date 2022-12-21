b8fe481291f6f58becf19062bcb0fcb640236f53
ocalm <ocal.mehmet@outlook.com.tr>
2022-12-21 17:20:24 +0300

    name className ile WebElement bulundu

---
+++ 10_Name className findElement findElements/src/test/java/com/vuealfasoftware/webapp/LoginTest.java
@@ -0,0 +1,73 @@
+package com.vuealfasoftware.webapp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class LoginTest {
+
+    @Test
+    public void LoginTest(){
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.manage().window().maximize();
+
+        //chromeDriver.get("https://vue-alfabank-demo.web.app/main");
+        chromeDriver.get("https://vue-alfabank-demo.web.app/login");
+
+        //WebElement emailAdresi = chromeDriver.findElement(By.id("userId"));
+        //WebElement sifre = chromeDriver.findElement(By.name("password"));
+
+        //emailAdresi.sendKeys("mehmet ocal");
+        //sifre.sendKeys("123456");
+
+        //WebElement emailAdresi = chromeDriver.findElement(By.className("input"));
+        //emailAdresi.sendKeys("mehmet ocal");
+
+        List<WebElement> inputList = chromeDriver.findElements(By.className("input"));
+        inputList.get(0).sendKeys("mehmet ocal");
+        inputList.get(1).sendKeys("123456");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        chromeDriver.quit();
+
+    }
+
+    /*
+    @Test
+    public  void LoginTestFirefox(){
+
+        System.setProperty("webdriver.geckodriver.driver", "src/main/resources/geckodriver.exe");
+
+        WebDriver fireFoxDriver = new FirefoxDriver();
+
+        fireFoxDriver.manage().window().maximize();
+
+        fireFoxDriver.get("https://vue-alfabank-demo.web.app/main");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        fireFoxDriver.quit();
+
+    }
+    */
+
+
+}
